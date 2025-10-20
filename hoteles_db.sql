3a5b78cf37a5af9f7ad7ae7fb6608e975b2cc2e7
AaronMon3 <aaronmonteagudo800@gmail.com>
2025-10-20 09:57:15 -0300

    Update hoteles_db.sql

--- hoteles_db.sql
+++ hoteles_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-10-2025 a las 04:02:43
+-- Tiempo de generación: 09-10-2025 a las 04:02:43
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
