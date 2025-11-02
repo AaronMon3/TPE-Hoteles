c6d1f087f4e657dc3ee19fe3bb23c4205c01ca4c
AaronMon3 <aaronmonteagudo800@gmail.com>
2025-11-02 20:10:57 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,30 +1,19 @@
+
+
 <?php
-// Configuración de Base de Datos
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'hoteles_db');
+const MYSQL_USER = 'root';
+const MYSQL_PASS = '';
+const MYSQL_DB   = 'hoteles_db';
+const MYSQL_HOST = 'localhost';
+const MYSQL_CHARSET = 'utf8';
 
-// Base URL del proyecto (se calcula automáticamente)
-$scriptName = $_SERVER['SCRIPT_NAME']; // Ejemplo: /hoteles/index.php o /TPE-Hoteles/index.php
-$baseUrl = dirname($scriptName); // Ejemplo: /hoteles o /TPE-Hoteles
-if ($baseUrl === '/') $baseUrl = ''; // Si está en la raíz, dejar vacío
-define('BASE_URL', $baseUrl);
+define('DB_USER', MYSQL_USER);
+define('DB_PASS', MYSQL_PASS);
+define('DB_NAME', MYSQL_DB);
+define('DB_HOST', MYSQL_HOST);
 
-try {
-    $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->query("SELECT 1 FROM tipohabitacion LIMIT 1");
-} catch (PDOException $e) {
-    $pdo = new PDO("mysql:host=".DB_HOST, DB_USER, DB_PASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    if(file_exists('hoteles_db.sql')){
-    $sql = file_get_contents('hoteles_db.sql');
-        $pdo->exec($sql);
-    } else {
-        die("Archivo SQL no encontrado");
-    }
-    $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-?>
+error_reporting(E_ALL);
+
+ini_set('display_errors', '1');
+
+
